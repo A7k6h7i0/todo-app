fbecb25894b05f4dc8faaed5d818162d1f3da3c8
Akhila <gantaakhila773@gmail.com>
2025-07-06 00:56:42 +0530

    Updated CORS configuration

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors())
+app.use(cors({
+      origin: 'https://todo-frontend-akhila-gantas-projects.vercel.app/'
+    }));
 app.use(express.json());
 
 let todos = []
